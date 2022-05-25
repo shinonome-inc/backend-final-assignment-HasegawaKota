546512315f11e467459bdb08bb459cdf09543edc
HasegawaKota <kota118kota@icloud.com>
2022-05-25 20:34:18 +0900

    SignUp機能のコミットです”

--- accounts/models.py
+++ accounts/models.py
@@ -2,8 +2,18 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# class User(AbstractUser):
-#     pass
+#デフォルトで用意されているUserではない
+class User(AbstractUser):
+    email=models.EmailField(blank=False,max_length=254)
+    yourname=models.CharField(blank=False,null=True,max_length=254)
 
-# class FriendShip(models.Model):
-#     pass
+    
+
+
+    class Meta:
+        db_table='User'
+
+
+
+class FriendShip(models.Model):
+     pass
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView,CreateView
+
+from accounts.models import User
+
+from .forms import SignupForm
+
+
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+    success_url = reverse_lazy('home')
+
+class HomeView(TemplateView):
+    template_name = 'accounts/home.html'
+
+class WelcomeView(TemplateView):
+    template_name = 'welcome/index.html'
+
+
