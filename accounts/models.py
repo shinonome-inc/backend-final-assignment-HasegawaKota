9bc8ce5378cf7eae8816e2d839009f369edd06f0
HasegawaKota <kota118kota@icloud.com>
2022-07-01 16:29:08 +0900

    '直しました'

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 
-from django.contrib.auth.models import AbstractUser,PermissionsMixin
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -8,7 +8,7 @@ from django.dispatch import receiver
 
 
 #デフォルトで用意されているUserではない
-class User(AbstractUser, PermissionsMixin):
+class User(AbstractUser):
     email = models.EmailField(max_length=254)
     
     class Meta:
